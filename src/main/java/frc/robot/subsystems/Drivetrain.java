32e81e0335fd02cf1d6727ec64bbdba789c64856
Jason Li <jasonli.0616@outlook.com>
2023-01-12 19:59:02 -0500

    Remove typo

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -64,7 +64,7 @@ public class Drivetrain extends SubsystemBase {
         configMotors();
 
         m_diffDrive = new DifferentialDrive(mot_leftFrontDrive, mot_rightFrontDrive);
-\
+
         // Instantiate CANCoders
         enc_leftDrive = new WPI_CANCoder(kDrivetrain.kCANCoder.id_leftEncoder);
         enc_rightDrive = new WPI_CANCoder(kDrivetrain.kCANCoder.id_rightEncoder);
