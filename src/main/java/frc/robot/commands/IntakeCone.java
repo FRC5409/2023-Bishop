22dbc3488b6d47dc0c2cd6916a236c09eaa166b1
Yonina W <100385243+yoninaW4917@users.noreply.github.com>
2023-02-02 16:21:27 -0500

    Update command intake cone

--- src/main/java/frc/robot/commands/IntakeCone.java
+++ src/main/java/frc/robot/commands/IntakeCone.java
@@ -5,28 +5,44 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
 
 public class IntakeCone extends CommandBase {
-  /** Creates a new IntakeCone. */
-  public IntakeCone() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
+	/** Creates a new intakeBall */
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
+	private final Intake m_intake;
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
+	public IntakeCone(Intake intake) {
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
+		m_intake = intake;
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(m_intake);
+	}
+
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		m_intake.intakeDown();
+		m_intake.stopRolling();
+	}
+
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		m_intake.rollForward();
+	}
+
+	// Called once the command ends or is interrupted
+	@Override
+	public void end(boolean interrupted) {
+		m_intake.stopRolling();
+		m_intake.intakeUp();
+	}
+
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		return false;
+	}
 }
