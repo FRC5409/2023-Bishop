7f2694e28028f453cf52ab42e6d1fa2670edfa0b
Ducvko <56515924+RsDrftr@users.noreply.github.com>
2023-02-19 18:53:10 -0500

    added so that once the arm rotates greater than 120 degrees the telescope will retract inwards then restore its previous position

--- src/main/java/frc/robot/commands/sequencing/RotateArmGroup.java
+++ src/main/java/frc/robot/commands/sequencing/RotateArmGroup.java
@@ -14,7 +14,7 @@ public class RotateArmGroup extends ParallelCommandGroup{
     public RotateArmGroup(Telescope sys_telescope, ArmPIDSubsystem sys_arm, double armDirection) {
         // Use addRequirements() here to declare subsystem dependencies.
         super(
-            Commands.either(new TelescopeTo(sys_telescope, kTelescope.kDestinations.kRetracted), Commands.waitSeconds(0), () -> Math.abs(armDirection - sys_arm.getMeasurement()) > 0.30),
+            Commands.either(new TelescopeTo(sys_telescope, kTelescope.kDestinations.kRetracted).andThen(new TelescopeTo(sys_telescope, sys_telescope.getPrevPos())), Commands.waitSeconds(0), () -> Math.abs(armDirection - sys_arm.getMeasurement()) > 0.30 && sys_telescope.getDistance() >= 3),
             new ArmRotation(sys_arm, armDirection)
         );
     }
