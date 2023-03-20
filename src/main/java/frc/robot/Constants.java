218b77fbb780936d0e284edb29857c1cf9c1432d
Jason Li <jasonli.0616@outlook.com>
2023-03-20 19:06:31 -0400

    Adjust values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -301,6 +301,7 @@ public final class Constants {
 
             public final static double kIdling                  = 0.96; // 48.5 inches
             public final static double kBalancing               = 1.21; // old 1.2 
+            public final static double kAutoDrivingWithCone     = 1.20;
 
             // setpoint of hardstop of shoulder side of robot: 0.513
             // measurement in inches is from the edge of the claw plate
--- src/main/java/frc/robot/commands/auto/OneConeOnePickupConeAuto.java
+++ src/main/java/frc/robot/commands/auto/OneConeOnePickupConeAuto.java
@@ -10,8 +10,10 @@ import com.pathplanner.lib.PathPlannerTrajectory;
 
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.kArmSubsystem;
 import frc.robot.Constants.kClaw;
 import frc.robot.Constants.kTelescope;
+import frc.robot.commands.arm.ArmRotation;
 import frc.robot.commands.arm.TelescopeTo;
 import frc.robot.commands.claw.ClawMovement;
 import frc.robot.subsystems.ArmPIDSubsystem;
@@ -33,14 +35,23 @@ public class OneConeOnePickupConeAuto extends SequentialCommandGroup {
 
                 new PlaceConeOnMidAtStart(sys_armPIDSubsystem, sys_telescope, sys_claw),
                 Commands.waitSeconds(1),
-                new AutoPathPlanning(sys_drivetrain, pathGroup.get(0)),
 
-                // Grab cone from ground
-                new TelescopeTo(sys_telescope, kTelescope.kDestinations.kGroundBack),
+                new AutoPathPlanning(sys_drivetrain, pathGroup.get(0))
+                    .alongWith(
+                        // Ready to grab cone
+                        new TelescopeTo(sys_telescope, kTelescope.kDestinations.kGroundBack)
+                    ),
+                
                 new ClawMovement(sys_claw, kClaw.coneClosePosition).withTimeout(1),
-                new TelescopeTo(sys_telescope, kTelescope.kDestinations.kRetracted),
 
-                new AutoPathPlanning(sys_drivetrain, pathGroup.get(1)),
+                // Drive to charge station
+                new AutoPathPlanning(sys_drivetrain, pathGroup.get(1))
+                    .alongWith(
+                        // Cone grabbed
+                        new TelescopeTo(sys_telescope, kTelescope.kDestinations.kRetracted).withTimeout(0.5),
+                        new ArmRotation(sys_armPIDSubsystem, kArmSubsystem.kSetpoints.kAutoDrivingWithCone).withTimeout(1)
+                    ),
+
                 new BalancingChargeStation(sys_drivetrain)
         );
     }
